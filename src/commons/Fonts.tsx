3599274fa1ae410115412d5a8982ec7224168b06
anne <heb2021@gmail.com>
2022-09-29 01:52:05 +0900

    style: add Fonts.tsx

---
+++ src/commons/Fonts.tsx
@@ -0,0 +1,11 @@
+export default {
+  G마켓Sans_Bold: 'GmarketSansTTFBold',
+  G마켓Sans_Light: 'GmarketSansTTFLight',
+  G마켓Sans_Medium: 'GmarketSansTTFMedium',
+  NotoSans_Bold: 'NotoSansCJKkr-Bold',
+  NotoSans_Light: 'NotoSansCJKkr-Light',
+  NotoSans_Regular: 'NotoSansCJKkr-Regular',
+  KoPubWorld돋움체_Bold: 'KoPubDotumBold',
+  KoPubWorld돋움체_Light: 'KoPubDotumLight',
+  KoPubWorld돋움체_Medium: 'KoPubDotumMedium',
+};
