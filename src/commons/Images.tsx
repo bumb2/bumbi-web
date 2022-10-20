0a3ce61586aebb26e0b80042abf4a7b384ce3b03
anne <heb2021@gmail.com>
2022-10-21 01:40:39 +0900

    fix: add ion-icon

--- src/commons/Images.tsx
+++ src/commons/Images.tsx
@@ -1,4 +1,5 @@
 export default {
+  THUMBNAIL: require('@assets/Images/thumbnail.png'),
   BUMBI_INSPIRED: require('@assets/Images/bumbi-inspired.png'),
   MEETPET_PARTNERS: require('@assets/Images/meetpet-partners.png'),
   SKILLS: require('@assets/Images/skills.png'),
